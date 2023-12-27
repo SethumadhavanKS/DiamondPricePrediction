8c84062753877145ef68ed4a4bb9064d7263bf45
SethumadhavanKS <zethumadhavansethu@gmail.com>
2023-12-27 14:29:16 +0530

    Created project structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'DiamondPricePrediction',
+    version='0.0.1',
+    author='Sethumadhavan',
+    author_email='zethumadhavansethu@gmail.com',
+    packages=find_packages()
+    )
---
+++ template.py
@@ -0,0 +1,42 @@
+import os
+from pathlib import Path
+packageName = 'DiamondPricePrediction'
+
+lstOFFiles = [
+    "github/workfloes/.gitkeep",
+    f"src/{packageName}/__init__.py",
+    f"src/{packageName}/components/__init__.py",
+    f"src/{packageName}/components/dataIngestion.py",
+    f"src/{packageName}/components/dataTransformation.py",
+    f"src/{packageName}/components/modelTrainer.py",
+    f"src/{packageName}/pipeline/__init__.py",
+    f"src/{packageName}/pipeline/trainingPipeline.py",
+    f"src/{packageName}/pipeline/predictionPipeline.py",
+    f"src/{packageName}/logger.py",
+    f"src/{packageName}/exception.py",
+    f"src/{packageName}/utils/__init__.py",
+    "notebooks/research.ipynb",
+    "notebooks/data/.gitkeep",
+    "requirements.txt",
+    "setup.py",
+    "init_setup.sh"
+]
+
+#Creating directory
+for filePath in lstOFFiles:
+    filePath = Path(filePath)
+    fileDir,fileName = os.path.split(filePath)
+
+    if fileDir != "":
+        os.makedirs(fileDir, exist_ok=True)
+        
+    #Creating file
+    if(not os.path.exists(filePath)) or (os.path.getsize(filePath)==0):
+        with open(filePath, "w") as f:
+            pass
+    else:
+        print("File already exists")
+
+
+
+
